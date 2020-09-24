477aec92c2a2e0edc544c6f2ba9054214ebf0b4c
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-09-24 17:00:19 -0500

    Menu pricipal y segundo prototipo
    Se terminó el menu principal y el segundo prototipo

---
+++ ProyectoUnity/PrototiposExploratorios/Assets/Scripts/SecondController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondController : MonoBehaviour
+{
+    public GameObject surface;
+
+    public void chageCubeFriction(float value){
+        gameObject.GetComponent<BoxCollider>().material.dynamicFriction = value;
+    }
+
+    public void chageSurfaceFriction(float value){
+        surface.GetComponent<BoxCollider>().material.dynamicFriction = value;
+    }
+
+    public void resetCube(){
+        gameObject.transform.localPosition = new Vector3(-6.16f, 4.49f, 0f);
+        gameObject.transform.localRotation = new Quaternion(0f, 0f, -20f, 0f);
+    }
+}
