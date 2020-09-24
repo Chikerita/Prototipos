30bfe08f315e61b66ad9661906b866a7c4b9a426
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-09-24 13:49:28 -0500

    Cambios en el primer prototipo
    Se realizo el primer prototipo

---
+++ ProyectoUnity/PrototiposExploratorios/Assets/Scripts/CubeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeController : MonoBehaviour
+{
+
+    public InputField height;
+
+    public void StartFall(){
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    public void ResetFall(){
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+        float value = float.Parse(height.text);
+        gameObject.transform.localPosition = new Vector3(0f, value, 0f);
+    }
+}
